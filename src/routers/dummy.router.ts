001be329ec87cc3106fc83d1dc6c076afb572bb7
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-08-04 21:06:25 -0300

    feat: Init nestjs app

--- src/routers/dummy.router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { getDummyMessage, getTestData } from "../controllers/dummy.controller";
-
-const dummyRouter = Router()
-
-dummyRouter.get("/message", getDummyMessage)
-dummyRouter.get("/test-data", getTestData)
-
-export default dummyRouter
--- src/routers/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import dummyRouter from "./dummy.router";
-
-const rootRouter = Router()
-
-rootRouter.use("/dummy", dummyRouter)
-
-export default rootRouter
