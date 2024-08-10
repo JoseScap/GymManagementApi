55cca766969c0e16bdd509be0495369e60bf94ee
Francisco Perez <franutn23@gmail.com>
2024-08-09 23:27:43 -0300

    Change to programatic form

--- src/migrations/1723251438564-003_AddTimeStamps.ts
+++ src/migrations/1723251438564-003_AddTimeStamps.ts
@@ -1,21 +1,27 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class _003AddTimeStamps1723251438564 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            ALTER TABLE members
-            ADD COLUMN createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-            ADD COLUMN updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-        `);
+        await queryRunner.addColumns("members", [
+            new TableColumn({
+                name: "createdAt",
+                type: "timestamp",
+                isNullable: false,
+                default: "CURRENT_TIMESTAMP"
+            }),
+            new TableColumn({
+                name: "updatedAt",
+                type: "timestamp",
+                isNullable: false,
+                default: "CURRENT_TIMESTAMP",
+                onUpdate: "CURRENT_TIMESTAMP"
+            })
+        ]);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            ALTER TABLE members
-            DROP COLUMN createdAt,
-            DROP COLUMN updatedAt
-        `);
+        await queryRunner.dropColumns("members", ["createdAt", "updatedAt"]);
     }
 
 }
